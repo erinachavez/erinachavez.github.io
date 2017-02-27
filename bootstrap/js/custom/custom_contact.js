eef503c72d96ec6628affab69d0d9143527adb98
erinachavez <Erina@172-26-244-221.DYNAPOOL.NYU.EDU>
2017-02-27 18:14:26 +0000

    contact form

--- bootstrap/js/custom/custom_contact.js
+++ bootstrap/js/custom/custom_contact.js
@@ -1,7 +1,7 @@
 $(document).ready( function(){
-	$("#submit").mouseenter(function(){
-	    $("#submit").removeClass("toggle_display_block");
-	    $("#submit").addClass("toggle_display_none");
+	$("#ss-submit").mouseenter(function(){
+	    $("#ss-submit").removeClass("toggle_display_block");
+	    $("#ss-submit").addClass("toggle_display_none");
 	    $("#submit2").removeClass("toggle_display_none");
 	    $("#submit2").addClass("toggle_display_block");
 	});
@@ -9,7 +9,7 @@ $(document).ready( function(){
 	$("#submit2").mouseleave(function(){
 	    $("#submit2").removeClass("toggle_display_block");
 	    $("#submit2").addClass("toggle_display_none");
-	    $("#submit").removeClass("toggle_display_none");
-	    $("#submit").addClass("toggle_display_block");
+	    $("#ss-submit").removeClass("toggle_display_none");
+	    $("#ss-submit").addClass("toggle_display_block");
 	});
 });
