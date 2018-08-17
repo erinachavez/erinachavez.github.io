a7045020949cef099f1840514019361536427c88
erinachavez <Erina@Erinas-MacBook-Pro.local>
2018-08-16 22:46:54 -0400

    big website update

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	// Navigation filter
+	$('polygon').mouseover(function(){
+		$(this).attr('filter','url(#f1)');
+	});
+
+	$('polygon').mouseout(function(){
+		$(this).removeAttr('filter');
+	});
+
+	$('h2').mouseover(function(){
+		h2id = $(this).attr('id');
+		h2id = '.' + h2id;
+		$(h2id).attr('filter','url(#f1)');
+	});
+
+	$('h2').mouseout(function(){
+		$('polygon').removeAttr('filter');
+	});
+});
+
+/* Home Type Animation */
+function typed_load(){
+	var typed = new Typed("#typeHere", {
+		stringsElement: '#typeText',
+		typeSpeed: 100,
+		backSpeed: 100,
+		backDelay: 750,
+		startDelay: 750,
+		loop: true,
+		smartBackspace: false
+	});
+
+	//Change Type Cursor to Match Current Font and Font Size
+	$(document).on('DOMSubtreeModified', '#typeHere', function(){
+		var currentFont = $('#typeHere span').css('font-family');
+		$('.typed-cursor').css('font-family',currentFont);
+
+		var currentFontSize = $('#typeHere span').css('font-size');
+		$('.typed-cursor').css('font-size',currentFontSize);
+
+		var currentFontColor = $('#typeHere span').css('color');
+		$('.typed-cursor').css('color',currentFontColor);
+	});
+}
