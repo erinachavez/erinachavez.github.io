6b8331ec74c95b4ebb34a18b3d602ef532eac75b
Erina Chavez <chavez.erina@gmail.com>
2019-07-21 19:37:41 -0400

    Mobile optimizationgit add .git add .

--- js/custom.js
+++ js/custom.js
@@ -1,47 +1,29 @@
-$(document).ready(function(){
+w3.includeHTML();
+
+$(document).ready(function() {
 	// Navigation filter
-	$('polygon').mouseover(function(){
-		$(this).attr('filter','url(#f1)');
+	$("nav polygon").mouseover(function() {
+		$(this).attr("filter","url('#f1')");
 	});
 
-	$('polygon').mouseout(function(){
-		$(this).removeAttr('filter');
+	$("nav polygon").mouseout(function() {
+		$(this).removeAttr("filter");
 	});
 
-	$('h2').mouseover(function(){
-		h2id = $(this).attr('id');
-		h2id = '.' + h2id;
-		$(h2id).attr('filter','url(#f1)');
+	$("nav h2").mouseover(function() {
+		target = $(this).attr("data-polygon-target");
+		$("#" + target).attr("filter","url('#f1')");
 	});
 
-	$('h2').mouseout(function(){
-		$('polygon').removeAttr('filter');
-	});
-
-	// Works
-	$('.main-carousel').flickity({
-		cellAlign: 'left',
-		contain: true,
-		wrapAround: true,
-		freeScroll: true,
-		pageDots: false,
-		prevNextButtons: false,
-		autoPlay: 2000
-	});
-
-	$('.main-carousel').mouseover(function(){
-		$('#alert').css('opacity','1');
-	});
-
-	$('.main-carousel').mouseleave(function(){
-		$('#alert').css('opacity','0');
+	$("nav h2").mouseout(function() {
+		$("nav polygon").removeAttr("filter");
 	});
 });
 
 /* Home Type Animation */
-function typed_load(){
+function typed_load() {
 	var typed = new Typed("#typeHere", {
-		stringsElement: '#typeText',
+		stringsElement: "#typeText",
 		typeSpeed: 100,
 		backSpeed: 100,
 		backDelay: 750,
@@ -51,14 +33,14 @@ function typed_load(){
 	});
 
 	//Change Type Cursor to Match Current Font and Font Size
-	$(document).on('DOMSubtreeModified', '#typeHere', function(){
-		var currentFont = $('#typeHere span').css('font-family');
-		$('.typed-cursor').css('font-family',currentFont);
+	$(document).on("DOMSubtreeModified", "#typeHere", function() {
+		var currentFont = $("#typeHere span").css("font-family");
+		$(".typed-cursor").css("font-family",currentFont);
 
-		var currentFontSize = $('#typeHere span').css('font-size');
-		$('.typed-cursor').css('font-size',currentFontSize);
+		var currentFontSize = $("#typeHere span").css("font-size");
+		$(".typed-cursor").css("font-size",currentFontSize);
 
-		var currentFontColor = $('#typeHere span').css('color');
-		$('.typed-cursor').css('color',currentFontColor);
+		var currentFontColor = $("#typeHere span").css("color");
+		$(".typed-cursor").css("color",currentFontColor);
 	});
 }
